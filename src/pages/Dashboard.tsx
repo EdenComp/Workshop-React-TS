f7db07e9f3edff48e732b14efb8eab55bf08bd07
Laure GAGNER <laure.gagner@epitech.eu>
2022-06-01 11:14:44 -0400

    [ADD] ImageList in DashoardPage
    Signed-off-by: Laure GAGNER <laure.gagner@epitech.eu>

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import TopBar from "../components/TopBar";
+import StandardImageList from "../components/StandardImageList";
 
 export default function Dashboard() : JSX.Element
 {
     return (
         <>
             <TopBar />
+            <StandardImageList/>
         </>
     )
 }
