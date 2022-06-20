d51553dd4fd95b8c914587ff05d232882eb8dad6
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-06-20 11:39:18 +0200

    fix: Useless instagram files removed

--- src/components/auth/ConnectButton.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '@mui/material';
-
-export default function ConnectButton() {
-	return <Button variant="contained">Connect</Button>;
-}
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,10 +1,8 @@
-import TopBar from 'components/TopBar';
 import StandardImageList from 'components/StandardImageList';
 
 export default function Dashboard(): JSX.Element {
 	return (
 		<>
-			<TopBar />
 			<StandardImageList />
 		</>
 	);
