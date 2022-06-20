211046c9fe5da9ca5ee37a356a8c435e32206e74
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-06-20 13:44:00 +0200

    feat: Simple task display

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
 
-import { Card, CardActions, CardContent } from '@mui/material';
-
-import Button from '@mui/material/Button';
+import { Card, CardContent } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
-export default function TaskCard(): JSX.Element {
+type TaskCardProps = {
+	title: string;
+	description: string;
+};
+
+export default function TaskCard({ title, description }: TaskCardProps): JSX.Element {
 	return (
 		<>
 			<Card sx={{ maxWidth: 400 }}>
 				<CardContent>
-					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-						Word of the Day
-					</Typography>
 					<Typography variant="h5" component="div">
-						benevolent
+						{title}
 					</Typography>
 					<Typography sx={{ mb: 1.5 }} color="text.secondary">
-						adjective
-					</Typography>
-					<Typography variant="body2">
-						well meaning and kindly.
-						<br />
-						{'"a benevolent smile"'}
+						{description}
 					</Typography>
 				</CardContent>
-				<CardActions>
-					<Button size="small">Learn More</Button>
-				</CardActions>
 			</Card>
 		</>
 	);
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,9 +1,30 @@
 import TaskCard from 'components/TaskCard';
 
+import Grid from '@mui/material/Grid';
+
 export default function Dashboard(): JSX.Element {
 	return (
 		<>
-			<TaskCard />
+			<Grid container rowSpacing={1} columnSpacing={{ xs: 1 }}>
+				{tasks.map((task) => (
+					<Grid item xs={3} key={task.id}>
+						<TaskCard title={task.title} description={task.description} />
+					</Grid>
+				))}
+			</Grid>
 		</>
 	);
 }
+
+const tasks = [
+	{
+		id: 1,
+		title: 'Task 1',
+		description: 'This is a task description',
+	},
+	{
+		id: 2,
+		title: 'Task 2',
+		description: 'This is another task description',
+	},
+];
