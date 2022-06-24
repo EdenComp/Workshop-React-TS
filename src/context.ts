8d0725b99b49e3d9b85385786fd0ace04928c1b5
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-06-24 12:17:02 +0200

    fix: ESLint wrong warning on context

--- src/context.ts
+++ src/context.ts
@@ -3,5 +3,5 @@ import { TaskType } from 'types/types';
 
 export const TaskContext = createContext({
 	tasks: [] as TaskType[],
-	setTasks: (tasks: TaskType[]) => {},
+	setTasks: (tasks: TaskType[]) => {}, // eslint-disable-line @typescript-eslint/no-unused-vars
 });
